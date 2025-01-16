057715f1fbdeab6e64e78c3e0c7a98f514e8ef6a
Danyl Brahin <inicerer141@gmail.com>
2025-01-16 21:47:23 +0200

    [PoC] Remove unused code

--- composeApp/src/iosMain/kotlin/com/dezdeqness/akane/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.dezdeqness.akane
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
