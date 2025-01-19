0fe1bc331c3544c6361952b5f4c013db7daa0cfc
Danyl Brahin <inicerer141@gmail.com>
2025-01-19 16:43:31 +0200

    [PoC] Add player for Android, rest - placeholder

---
+++ features/videoplayer/src/commonMain/kotlin/com/dezdeqness/videoplayer/VideoPlayer.kt
@@ -0,0 +1,6 @@
+package com.dezdeqness.videoplayer
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun VideoPlayerScreen(videoUrl: String)
---
+++ features/videoplayer/src/iosMain/kotlin/com/dezdeqness/videoplayer/VideoPlayer.kt
@@ -0,0 +1,13 @@
+package com.dezdeqness.videoplayer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+actual fun VideoPlayerScreen(videoUrl: String) {
+    Box(modifier = Modifier.fillMaxSize().background(Color.Red))
+}
