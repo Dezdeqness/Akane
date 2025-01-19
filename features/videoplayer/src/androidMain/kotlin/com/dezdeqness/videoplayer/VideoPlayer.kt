b8c01768fa5022cce4393ad26e9a407187f171a5
Danyl Brahin <inicerer141@gmail.com>
2025-01-19 19:37:03 +0200

    [PoC] Implement player for Android

--- features/videoplayer/src/androidMain/kotlin/com/dezdeqness/videoplayer/VideoPlayer.kt
+++ features/videoplayer/src/androidMain/kotlin/com/dezdeqness/videoplayer/VideoPlayer.kt
@@ -1,21 +1,26 @@
 package com.dezdeqness.videoplayer
 
+import android.view.TextureView
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.uri.Uri
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.PlayerView
 
 @Composable
 actual fun VideoPlayerScreen(videoUrl: String) {
     val context = LocalContext.current
+
+    val playerView = remember { TextureView(context) }
 
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
@@ -30,14 +35,18 @@ actual fun VideoPlayerScreen(videoUrl: String) {
 
     DisposableEffect(Unit) {
         onDispose {
+            exoPlayer.clearVideoTextureView(playerView)
+            exoPlayer.stop()
             exoPlayer.release()
         }
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
         AndroidView(
-            modifier = Modifier.fillMaxSize(),
-            factory = { PlayerView(context).apply { player = exoPlayer } }
+            modifier = Modifier.fillMaxSize().aspectRatio(16 / 9F),
+            factory = { playerView.apply {
+                exoPlayer.setVideoTextureView(playerView)
+            } }
         )
     }
 }
