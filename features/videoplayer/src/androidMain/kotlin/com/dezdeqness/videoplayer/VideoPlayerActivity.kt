c720eb9769f58a60423e3dd3d91ca11ad56586a7
Danyl Brahin <inicerer141@gmail.com>
2025-02-19 21:56:39 +0200

    [PoC] On Android implement mechanizm of show/hide of system elements

--- features/videoplayer/src/androidMain/kotlin/com/dezdeqness/videoplayer/VideoPlayerActivity.kt
+++ features/videoplayer/src/androidMain/kotlin/com/dezdeqness/videoplayer/VideoPlayerActivity.kt
@@ -7,7 +7,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowCompat
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.dezdeqness.designsystem.utils.noRippleClickable
+import com.dezdeqness.videoplayer.core.InstallFullScreenState
+import com.dezdeqness.videoplayer.core.SystemBarsVisibility
+import com.dezdeqness.videoplayer.core.rememberFullScreenState
 import com.dezdeqness.videoplayer.navigation.EPISODE_URL
 
 class VideoPlayerActivity : ComponentActivity() {
@@ -15,14 +25,32 @@ class VideoPlayerActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        WindowCompat
-            .getInsetsController(window, window.decorView)
-            .isAppearanceLightStatusBars = true
 
         val url = intent.getStringExtra(EPISODE_URL) ?: return
 
         setContent {
-            VideoPlayerScreen(videoUrl = url)
+            val systemBarsControllerState = rememberFullScreenState(
+                initialNavigationBarVisibility = SystemBarsVisibility.Visible,
+                initialStatusBarVisibility = SystemBarsVisibility.Visible
+            )
+
+            InstallFullScreenState(systemBarsControllerState)
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier
+                    .background(Color.Black)
+                    .fillMaxSize()
+                    .noRippleClickable {
+                        if (systemBarsControllerState.isSystemBarVisible) {
+                            systemBarsControllerState.hideSystemBar()
+                        } else {
+                            systemBarsControllerState.showSystemBar()
+                        }
+                    }
+            ) {
+                VideoPlayerScreen(videoUrl = url)
+            }
         }
     }
 
