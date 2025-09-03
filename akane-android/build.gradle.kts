4e10c62ddb90a31541e18f57981b8238641dadb7
Danyl Brahin <inicerer141@gmail.com>
2025-09-03 12:17:30 +0300

    [PoC] Configure release build

--- akane-android/build.gradle.kts
+++ akane-android/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import kotlin.apply
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
@@ -20,11 +23,46 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
+    signingConfigs {
+        create("release") {
+            val props = Properties().apply {
+                val file = rootProject.file("local.properties")
+                load(file.inputStream())
+            }
+
+            keyAlias = props["keystore.key.alias"] as String
+            keyPassword = props["keystore.key.password"] as String
+            storeFile = file(props["keystore.release"] as String)
+            storePassword = props["keystore.password"] as String
         }
     }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+
+        getByName("debug") {
+            isMinifyEnabled = false
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+        }
+
+        create("qa") {
+            isMinifyEnabled = true
+            isDebuggable = false
+            applicationIdSuffix = ".test"
+            matchingFallbacks.add("debug")
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
