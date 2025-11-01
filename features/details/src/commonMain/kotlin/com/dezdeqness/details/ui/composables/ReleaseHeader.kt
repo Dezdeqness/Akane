24e0d32b3898bcba8969a699962e1296fb972d24
Danyl Brahin <inicerer141@gmail.com>
2025-11-01 21:51:37 +0200

    v1.1.0: Refactor details header part 2

--- features/details/src/commonMain/kotlin/com/dezdeqness/details/ui/composables/ReleaseHeader.kt
+++ features/details/src/commonMain/kotlin/com/dezdeqness/details/ui/composables/ReleaseHeader.kt
@@ -1,68 +1,162 @@
 package com.dezdeqness.details.ui.composables
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dezdeqness.core.ui.theme.AppTheme
 import com.dezdeqness.core.ui.views.image.AppImage
+import com.dezdeqness.designsystem.nestedscroll.LocalNestedScrollConnection
 import com.dezdeqness.details.ui.model.ReleaseDetailsHeaderUiModel
+
+private val standardEnter = fadeIn(
+    tween(
+        durationMillis = 250,
+        delayMillis = 0,
+        easing = CubicBezierEasing(0.05f, 0.7f, 0.1f, 1.0f),
+    )
+)
+
+private val standardExit = fadeOut(
+    tween(
+        durationMillis = 200,
+        delayMillis = 0,
+        easing = CubicBezierEasing(0.3f, 0.0f, 1f, 1f),
+    )
+)
 
 @Composable
 fun ReleaseHeader(
     modifier: Modifier = Modifier,
     header: ReleaseDetailsHeaderUiModel,
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxWidth()
+    val connection = LocalNestedScrollConnection.current
+    val isScrollPass = remember {
+        derivedStateOf {
+            connection.progress > 0.7f
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(250.dp)
     ) {
         AppImage(
             data = header.imageUrl,
-            modifier = Modifier
-                .padding(top = 56.dp)
-                .height(250.dp)
-                .aspectRatio(3 / 4f)
-                .clip(RoundedCornerShape(8.dp))
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
 
-        Text(
-            header.title,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
-            color = AppTheme.colors.textPrimary,
-            textAlign = TextAlign.Center,
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.White.copy(alpha = 0.7f),
+                            Color.White.copy(alpha = 0.8f),
+                            Color.White.copy(alpha = 0.9f),
+                        ),
+                    )
+                )
         )
-        
-        Row(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
+
+        AnimatedVisibility(
+            visible = isScrollPass.value,
+            enter = standardEnter,
+            exit = standardExit,
         ) {
-            if (header.season.isNotEmpty()) {
-                Text(
-                    header.season,
-                    fontSize = 14.sp,
-                    color = AppTheme.colors.textSecondary,
-                )
-            }
-            if (header.year.isNotEmpty()) {
-                Text(
-                    " • ${header.year}",
-                    fontSize = 14.sp,
-                    color = AppTheme.colors.textSecondary,
-                )
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(120.dp)
+                        .aspectRatio(2f / 3f)
+                        .clip(RoundedCornerShape(8.dp))
+                        .border(
+                            width = 1.dp,
+                            color = AppTheme.colors.surface.copy(alpha = 0.5f),
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                ) {
+                    AppImage(
+                        data = header.imageUrl,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 16.dp, bottom = 8.dp),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+
+                    Text(
+                        header.title,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = AppTheme.colors.textPrimary,
+                        maxLines = 3,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row {
+                        if (header.season.isNotEmpty()) {
+                            Text(
+                                header.season,
+                                fontSize = 14.sp,
+                                color = AppTheme.colors.textSecondary,
+                            )
+                        }
+                        if (header.year.isNotEmpty()) {
+                            Text(
+                                " • ${header.year}",
+                                fontSize = 14.sp,
+                                color = AppTheme.colors.textSecondary,
+                            )
+                        }
+                    }
+                }
             }
         }
     }
--- features/details/src/commonMain/kotlin/com/dezdeqness/details/ui/composables/ReleaseToolbar.kt
+++ features/details/src/commonMain/kotlin/com/dezdeqness/details/ui/composables/ReleaseToolbar.kt
@@ -3,9 +3,12 @@ package com.dezdeqness.details.ui.composables
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
@@ -13,6 +16,7 @@ import com.dezdeqness.core.ui.theme.AppTheme
 import com.dezdeqness.core.ui.views.buttons.AppIconButton
 import com.dezdeqness.core.ui.views.toolbar.AppToolbar
 import com.dezdeqness.designsystem.icons.AkaneIcons
+import com.dezdeqness.designsystem.nestedscroll.LocalNestedScrollConnection
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,22 +27,37 @@ fun ReleaseToolbar(
     onBackPressed: () -> Unit,
     onFavouriteClicked: () -> Unit,
 ) {
+    val connection = LocalNestedScrollConnection.current
+    val isScrollPass = remember {
+        derivedStateOf {
+            connection.progress > 0.7f
+        }
+    }
+
+
     AppToolbar(
         modifier = modifier,
         title = {
-            Text(
-                title,
-                color = AppTheme.colors.textPrimary,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-
+            if (isScrollPass.value.not()) {
+                Text(
+                    title,
+                    color = AppTheme.colors.textPrimary,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
         },
         navigation = {
             AppIconButton(
-                icon = Icons.AutoMirrored.Filled.ArrowBack,
                 onClick = onBackPressed,
-            )
+                contentColor = AppTheme.colors.surface,
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = null,
+                    tint = AppTheme.colors.textPrimary
+                )
+            }
         },
         colors = TopAppBarDefaults
             .topAppBarColors()
@@ -47,10 +66,15 @@ fun ReleaseToolbar(
             ),
         actions = {
             AppIconButton(
-                icon = if (isFavourite) AkaneIcons.Favorite else AkaneIcons.FavoriteBorder,
                 onClick = onFavouriteClicked,
-                tint = AppTheme.colors.textPrimary,
-            )
+                contentColor = AppTheme.colors.surface,
+            ) {
+                Icon(
+                    if (isFavourite) AkaneIcons.Favorite else AkaneIcons.FavoriteBorder,
+                    contentDescription = null,
+                    tint = AppTheme.colors.textPrimary
+                )
+            }
         }
     )
 }
