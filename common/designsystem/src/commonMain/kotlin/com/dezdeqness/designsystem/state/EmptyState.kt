4f1100dda7cd7beaef7617403249dc7583513fef
Danyl Brahin <inicerer141@gmail.com>
2025-12-17 21:50:50 +0200

    v1.1.0: Update core-ui version, create states

---
+++ common/designsystem/src/commonMain/kotlin/com/dezdeqness/designsystem/state/EmptyState.kt
@@ -0,0 +1,50 @@
+package com.dezdeqness.designsystem.state
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.dezdeqness.core.ui.views.state.StateButtonConfig
+import com.dezdeqness.core.ui.views.state.StateButtonStyle
+import com.dezdeqness.core.ui.views.state.StateView
+
+@Composable
+fun EmptyState(
+    modifier: Modifier = Modifier,
+    title: String,
+    message: String? = null,
+    image: @Composable () -> Unit,
+    buttonTitle: String,
+    onClick: () -> Unit,
+) {
+    StateView(
+        modifier = modifier,
+        title = title,
+        message = message,
+        image = image,
+        buttons = buildList {
+            add(
+                StateButtonConfig(
+                    title = buttonTitle,
+                    style = StateButtonStyle.Text,
+                    onClick = onClick,
+                )
+            )
+        },
+    )
+}
+
+@Composable
+fun EmptyState(
+    modifier: Modifier = Modifier,
+    title: String,
+    message: String? = null,
+    image: @Composable () -> Unit,
+    buttonsContent: @Composable () -> Unit,
+) {
+    StateView(
+        modifier = modifier,
+        title = title,
+        message = message,
+        image = image,
+        buttonsContent = buttonsContent,
+    )
+}
